8a91054235a2fc710e214a8c5736080076b168b5
loico square <loicsan07@gmail.com>
2022-07-23 05:24:24 +0100

    All ok;

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapsService } from '../services/face-snaps.service';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss'],
+})
+export class FaceSnapComponent implements OnInit {
+  @Input() faceSnap!: FaceSnap;
+
+  buttonText!: string;
+
+  constructor(private faceSnapService: FaceSnapsService, private route : Router) {}
+
+  ngOnInit() {
+    this.buttonText = 'Oh Snap!';
+  }
+
+  onSnap() {
+    if (this.buttonText === 'Oh Snap!') {
+      this.faceSnapService.snapFaceSnapById(this.faceSnap.id, 'snap');
+      this.buttonText = 'Oops, unSnap!';
+    } else {
+      this.faceSnapService.snapFaceSnapById(this.faceSnap.id, 'unsnap');
+      this.buttonText = 'Oh Snap!';
+    }
+  }
+
+  onViewFaceSnap(){
+    this.route.navigateByUrl(`facesnap/${this.faceSnap.id}`);
+  }
+}
